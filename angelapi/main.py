305d005a93fd23ba19c60728794b806bd95cc403
Amir <93888864+AmirFone@users.noreply.github.com>
2024-03-23 17:03:03 -0400

    Update main.py

--- angelapi/main.py
+++ angelapi/main.py
@@ -1,32 +1,25 @@
 import json
-from fastapi import FastAPI
+from fastapi import FastAPI, status
+from fastapi.responses import JSONResponse
 import uvicorn
 from mangum import Mangum
-from fastapi.middleware.cors import CORSMiddleware
-
+from security_analyzer import analyze_github_repo
 app = FastAPI()
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],  # Specify domains you want to allow
-    allow_credentials=True,
-    allow_methods=["*"],  # Specify HTTP methods to allow
-    allow_headers=["*"],
-)
-
-
-@app.post("/")
+@app.get("/")
 async def read_root():
     return {"Hello": "World"}
 
-
 @app.get("/endpoint")
-async def train_model():
-    return {"message": "whatever"}
-
+async def process_link(link: str):
+    print("link:",link) 
+    # Analyze the GitHub repository and get the result
+    result = analyze_github_repo(link)
+    
+    # Return the result as JSON response with 200 OK status code
+    return JSONResponse(content=result, status_code=status.HTTP_200_OK)
 
 handler = Mangum(app)
-
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
