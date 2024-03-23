360ccadbea2e6254bb5ed937a2a754667cf1de2f
Philip Tempelman <tempelmanphilip@gmail.com>
2024-03-23 12:55:40 -0400

    set up functionality framework api

---
+++ angelapi/functionality.py
@@ -0,0 +1,14 @@
+
+
+def create_claude_api_call_with_file_content():
+    prompt = ...
+    pass
+
+def filenames_and_content_from_repo():
+    pass
+
+def feed_back_to_nextjs(status):
+    # where status is working on a file (executing api call)
+    # or results when it's done (json report)
+    
+    pass
